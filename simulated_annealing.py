7c8c698a1e27e230d5f8f2fce47bbc6b60aca90d
Emanuele Olivetti <olivetti@fbk.eu>
2014-03-11 10:53:53 +0100

    fixed minor issue about k=0 + improved printout

--- simulated_annealing.py
+++ simulated_annealing.py
@@ -21,11 +21,11 @@ def temperature_cauchy(k, T0):
 def temperature_boltzmann(k, T0):
     """Boltzmann schedule to update tempertature at step k.
     """
-    T_new = T0 / np.log(1 + k)
+    T_new = T0 / np.log(1 + k+1)
     return T_new
 
 
-def anneal(initial_state, energy_function, neighbour, transition_probability, temperature, max_steps, energy_max, T0):
+def anneal(initial_state, energy_function, neighbour, transition_probability, temperature, max_steps, energy_max, T0, log_every=1000):
     """Simulated annealing optimization.
 
     initial_state
@@ -41,6 +41,7 @@ def anneal(initial_state, energy_function, neighbour, transition_probability, te
     state_best = state
     energy_best = energy
     k = 0
+    print "Step) Energy \t Prob. \t Temp. \t (E'-E) \t BEST"
     while k < max_steps and energy > energy_max:
         T = temperature(k, T0)
         state_new = neighbour(state)
@@ -50,18 +51,16 @@ def anneal(initial_state, energy_function, neighbour, transition_probability, te
             state = state_new
             energy_old = energy
             energy = energy_new
-            print "%s) %s \t %s \t %s \t %s \t %s" % (k, energy, p, T, energy_new - energy_old, energy_best)
-
-
+            
         if energy_new < energy_best:
             state_best = state_new
             energy_best = energy_new
-            # print "%s) %s \t %s \t %s \t %s \t %s" % (k, energy_best, p, T, energy_old, energy_new)
+            print "* %s) %s \t %s \t %s \t %s \t %s" % (k, energy_best, p, T, energy_old, energy_new)
 
-        # if (k % 1000) == 0:
-        #     print k, ")", energy_best, p
-        #     stdout.flush()
-
+        if (k % log_every) == 0:
+            print "%s) %s \t %s \t %s \t %s \t %s" % (k, energy, p, T, energy_new - energy_old, energy_best)
+            stdout.flush()
+            
         k += 1
 
     return state_best, energy_best
